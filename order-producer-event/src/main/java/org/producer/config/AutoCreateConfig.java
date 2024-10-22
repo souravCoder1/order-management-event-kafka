c17752ca33ab30ff2a794ac37e6d9322a1386664
souravpal01 <souravpal7217@gmail.com>
2024-10-22 14:19:32 +0530

    add topic local setup

---
+++ order-producer-event/src/main/java/org/producer/config/AutoCreateConfig.java
@@ -0,0 +1,26 @@
+package org.producer.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Profile("local")
+public class AutoCreateConfig {
+
+    @Bean
+    public NewTopic libraryEvents(){
+        return TopicBuilder.name("order-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+}
